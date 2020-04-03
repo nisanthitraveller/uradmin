67c403b66735c4aeebb9aa7bb9a6a450fdb0d841
nisanthitraveller <nisanth.kumar@itraveller.com>
2020-04-03 20:48:41 +0530

    changes in migration for adding audio token

---
+++ database/migrations/2020_04_03_132100_change_audio_token_team_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeAudioTokenTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team_members', function (Blueprint $table) {
+            $table->text('audio_token')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team_members', function (Blueprint $table) {
+            //
+        });
+    }
+}
