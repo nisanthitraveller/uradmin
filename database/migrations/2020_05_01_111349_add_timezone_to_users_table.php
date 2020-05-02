65aee99bf197390ffcf18ba35634c2291d1db467
nisanthitraveller <nisanth.kumar@itraveller.com>
2020-05-02 10:24:08 +0530

    DB migration

---
+++ database/migrations/2020_05_01_111349_add_timezone_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTimezoneToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('timezone')->nullable();
+            $table->string('timezone_offset')->nullable();
+            $table->tinyInteger('subscription')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
