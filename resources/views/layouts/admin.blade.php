1281d66f387a69616933f2c524d92abaff72c363
nisanthitraveller <nisanth.kumar@itraveller.com>
2020-05-11 18:38:30 +0530

    favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ trans('global.site_title') }}</title>
-    <link rel="shortcut icon" href="{{ asset('images/fav-icon.png') }}">
+    <link rel="shortcut icon" href="https://unremot.com/images/fav-icon.png">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
