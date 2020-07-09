8c9fcebcbd645ebe1306e3041ce12ffb09157505
Nisanth Kumar <nisanth.kumar@itraveller.com>
2020-07-09 15:34:23 +0530

    changes

---
+++ database/migrations/2020_07_09_093356_create_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ip_address', 250)->nullable();
+            $table->tinyInteger('home')->nullable();
+            $table->tinyInteger('signed_up')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
