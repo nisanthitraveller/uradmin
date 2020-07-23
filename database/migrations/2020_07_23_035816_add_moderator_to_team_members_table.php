419eed2dafb5afe1e9976f14db2f1b5c67b134b0
Nisanth Kumar <nisanth.kumar@itraveller.com>
2020-07-23 10:45:04 +0530

    changes

---
+++ database/migrations/2020_07_23_035816_add_moderator_to_team_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddModeratorToTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team_members', function (Blueprint $table) {
+            $table->tinyInteger('moderator')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team_members', function (Blueprint $table) {
+            //
+        });
+    }
+}
